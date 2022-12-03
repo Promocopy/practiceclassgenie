c3085babbdf51911261d818d19289a1c149b241a
promocopy <tobijosh4@gmail.com>
2022-12-03 14:42:01 +0100

    Add project files.

---
+++ practiceclassgenie/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace practice
+{
+
+    public class Ifstatement
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine(" Please enter a number");
+            int username= int.Parse(Console.ReadLine());
+
+            if (username == 1)
+            {
+                Console.WriteLine("your input is one");
+            }
+
+            else if (username == 2)
+            {
+                Console.WriteLine("your input is two");
+            }
+
+            else if (username == 3)
+            {
+                Console.WriteLine("your input is three");
+            }
+            else
+            {
+                Console.WriteLine("your input is not between 1-3");
+            }
+        }
+    }
+}
